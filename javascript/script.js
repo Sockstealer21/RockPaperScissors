3c85da06ce9c8f2d3b2d11a7e5e22454cabf7866
Sockstealer21 <michaelsuushi@gmail.com>
2022-06-01 17:07:52 -0700

    Replace index with vsc html layout and made computerPlay()

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,14 @@
+function computerPlay() {
+    let botRPS;
+    let randomSelection = Math.floor(Math.random()*100 );
+    if (randomSelection <= 33){
+        botRPS = 'Rock'
+    } else if (randomSelection <=66) {
+        botRPS = 'Paper'
+    } else {
+        botRPS = 'Scissors'
+    }
+    return botRPS
+}
 
+computerPlay()
