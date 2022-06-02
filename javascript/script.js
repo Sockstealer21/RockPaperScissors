5ccdf267939f05b1208f8b5ccd0a7aaa51dfc1ae
Sockstealer21 <michaelsuushi@gmail.com>
2022-06-01 21:45:04 -0700

    Complete singleRound function with all permutations of game end

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,40 @@
+const playerRPS = "rock";
+const computerSelection = computerPlay();
+
 function computerPlay() {
-    let botRPS;
+    let computerSelection
     let randomSelection = Math.floor(Math.random()*100 );
     if (randomSelection <= 33){
-        botRPS = 'Rock'
+        computerSelection = 'ROCK'
     } else if (randomSelection <=66) {
-        botRPS = 'Paper'
+        computerSelection = 'PAPER'
     } else {
-        botRPS = 'Scissors'
+        computerSelection = 'SCISSORS'
     }
-    return botRPS
+    return computerSelection
 }
 
-computerPlay()
+
+function singleRound(playerRPS, computerSelection) {
+    
+    playerRPS = playerRPS.toUpperCase()
+    if (playerRPS === computerSelection){    
+        result = 'It\'s a tie!';
+    } else if (playerRPS === 'ROCK' && computerSelection === 'PAPER') {
+        result = 'You Lose! Paper beats Rock';
+    } else if (playerRPS === 'ROCK' && computerSelection === 'SCISSORS'){
+        result = 'You win! Rock beats Scissors'
+    } else if (playerRPS === 'PAPER' && computerSelection === 'ROCK') {
+        result = 'You win! Paper beats Rock'
+    } else if (playerRPS === 'PAPER' && computerSelection === 'SCISSORS'){q
+    } else if (playerRPS === 'SCISSORS' && computerSelection === 'ROCK') {
+        result = 'You Lose! Rock beats Scissors'
+    } else if (playerRPS === 'SCISSORS' && computerSelection === 'PAPER') {
+        result = 'You Win! Scissors beats Paper'
+    }
+    return result;
+}
+
+console.log(singleRound(playerRPS, computerSelection));
+
+console.log(computerSelection)
